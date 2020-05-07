24ff6b679fdf0446c5be28e29fcf1c7ec2fde8b7
delicacylee <delicacylee@vip.sina.com>
2020-05-07 16:30:32 +0800

    更新目录
    更新目录，改用TypeScript语法

---
+++ SanguoSLG/assets/Scripts/GridUnitData.ts
@@ -0,0 +1,11 @@
+import { GridType } from './GridType'
+
+export class GridUnitData {
+    public gridType: GridType;
+    public gridPosition: cc.Vec2;
+    public localPosition: cc.Vec3;
+
+    public Distance(target: GridType) {
+            
+    };
+}
